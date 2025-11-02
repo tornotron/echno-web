0ebec0fa648543de0708aa37113db781ed77cb8f
Abin K Johny <abinkjohny2011@gmail.com>
2025-11-03 01:20:29 +0530

    Refactor utility imports to use tailwind-utils for consistency across UI components

--- components/ui/sonner.tsx
+++ components/ui/sonner.tsx
@@ -17,7 +17,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
     <Sonner
       theme={theme as ToasterProps["theme"]}
       className="toaster group"
-      position="top-right"
+      position="bottom-center"
       icons={{
         success: <CircleCheckIcon className="size-4" />,
         info: <InfoIcon className="size-4" />,
