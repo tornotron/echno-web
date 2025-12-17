b026363522441877603cd3a75fd0d9fa30c2d6fc
Abin K Johny <abinkjohny2011@gmail.com>
2025-12-17 17:24:44 +0530

    fix: add error and expiresAt properties to Session and JWT interfaces

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -7,6 +7,7 @@ declare module "next-auth" {
     idToken?: string
     refreshToken?: string
     provider?: string
+    error?: string
 
     user: {
       id: string
@@ -30,5 +31,7 @@ declare module "next-auth/jwt" {
     idToken?: string
     refreshToken?: string
     provider?: string
+    expiresAt?: number
+    error?: string
   }
 }
