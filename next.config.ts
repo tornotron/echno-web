d3baf8b7c6c38548bdd17085a6b62858067623e8
Abin K Johny <abinkjohny2011@gmail.com>
2026-01-29 13:23:28 +0530

    feat(config): add additional remote pattern for image loading from DigitalOcean Spaces

--- next.config.ts
+++ next.config.ts
@@ -19,6 +19,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'echno-object-store.blr1.digitaloceanspaces.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   async redirects() {
