2ca0794cbadbf0b82e847e40014a1356bd1f1498
Abin K Johny <abinkjohny2011@gmail.com>
2026-02-20 00:18:46 +0530

    feat: remove access request components and related files

--- components/access-request/index.ts
+++
@@ -1,4 +0,0 @@
-export { RequestAccessModal } from './request-access-modal';
-export { AccessRequestStatusBadge } from './access-request-status-badge';
-export { AccessRequestCard } from './access-request-card';
-export { LockedFeatureOverlay } from './locked-feature-overlay';
