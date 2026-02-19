602d01461e9d6fcaba8a8a1bbfbab26f24abdf63
Abin K Johny <abinkjohny2011@gmail.com>
2026-02-20 01:01:47 +0530

    refactor: remove admin layout and root page components

--- app/admin/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { AppLayout } from '@/components/common/app-layout';
-
-export default function AdminLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <AppLayout>{children}</AppLayout>;
-}
