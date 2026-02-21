9dd92fd3427b858728d367ca365f6517535c3aec
Abin K Johny <abinkjohny2011@gmail.com>
2026-02-21 19:51:09 +0530

    refactor: add TaskAttachmentsSection and TaskAttachmentsUploader components for task attachment management

--- features/projects/components/index.ts
+++ features/projects/components/index.ts
@@ -2,4 +2,3 @@ export { ProjectEditForm } from './project-edit-form';
 export { TeamMembersSection } from './team-members-section';
 export { AttachmentsSection } from './attachments-section';
 export { AttachmentsUploader } from './attachments-uploader';
-export { TaskAttachmentsUploader } from './task-attachments-uploader';
