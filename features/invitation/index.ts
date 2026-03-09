1effd47270b2df531bb05e63df133a377c0bf775
Abin K Johny <abinkjohny2011@gmail.com>
2026-03-10 00:20:15 +0530

    refactor(invitations): modularize InvitationsPage by extracting InvitationHeader and InvitationStats components

--- features/invitation/index.ts
+++ features/invitation/index.ts
@@ -5,3 +5,5 @@ export {
 export { InvitationStatusBadge } from './components/invitation-status-badge';
 export { InvitationAvatar } from './components/invitation-avatar';
 export { InvitationTable } from './components/invitation-table';
+export { InvitationHeader } from './components/invitation-header';
+export { InvitationStats } from './components/invitation-stats';
