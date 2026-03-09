f85f374c2b7dc811ccb8507da1de7eddf4f9dea0
Abin K Johny <abinkjohny2011@gmail.com>
2026-03-09 20:39:16 +0530

    refactor(invitation): create index file to consolidate invitation component exports

---
+++ features/invitation/index.ts
@@ -0,0 +1,7 @@
+export {
+  InvitationQRCode,
+  InvitationQRCodeDialog,
+} from './components/invitation-qr-code';
+export { InvitationStatusBadge } from './components/invitation-status-badge';
+export { InvitationAvatar } from './components/invitation-avatar';
+export { InvitationTable } from './components/invitation-table';
