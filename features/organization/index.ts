ba3eaed472c42915400121c030204b2e1194eb58
Abin K Johny <abinkjohny2011@gmail.com>
2026-03-09 20:41:13 +0530

    refactor(organization): add dialog components for logo management and enhance organization form

---
+++ features/organization/index.ts
@@ -0,0 +1,7 @@
+export { OrganizationCard } from './components/organization-card';
+export { OrganizationForm } from './components/organization-form';
+export { OrganizationSelector } from './components/organization-selector';
+export {
+  RemoveOrgLogoDialog,
+  SaveOrganizationDialog,
+} from './components/organization-alert-dialogs';
--- features/organization/insex.ts
+++
@@ -1,3 +0,0 @@
-export * from './organization-card';
-export * from './organization-form';
-export * from './organization-selector';
