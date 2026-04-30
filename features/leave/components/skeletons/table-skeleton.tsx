5f30ac31d788033ec662f88334d790754814d4f8
Abin K Johny <abinkjohny2011@gmail.com>
2026-04-30 12:38:21 +0530

    refactor: update leave skeleton components imports to use shadcn directory

--- features/leave/components/skeletons/table-skeleton.tsx
+++ features/leave/components/skeletons/table-skeleton.tsx
@@ -1,5 +1,5 @@
-import { Card, CardContent, CardHeader } from '@/components/ui/card';
-import { Skeleton } from '@/components/ui/skeleton';
+import { Card, CardContent, CardHeader } from '@/components/shadcn/card';
+import { Skeleton } from '@/components/shadcn/skeleton';
 
 interface TableSkeletonProps {
   statCount?: number;
