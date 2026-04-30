60ca2f40e279963bd52758ef11837694bb632f0b
Abin K Johny <abinkjohny2011@gmail.com>
2026-04-30 11:37:58 +0530

    Refactor: Changed directory shadcn to ui

--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -1,2 +1,13 @@
-'use client';
-export * from '@/components/shadcn/skeleton';
+import { cn } from '@/lib/utils/index';
+
+function Skeleton({ className, ...props }: React.ComponentProps<'div'>) {
+  return (
+    <div
+      data-slot="skeleton"
+      className={cn('bg-accent animate-pulse rounded-md', className)}
+      {...props}
+    />
+  );
+}
+
+export { Skeleton };
