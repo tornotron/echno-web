f35aa500a9e9e6d27b75667df49b477398ce1b0d
Abin K Johny <abinkjohny2011@gmail.com>
2026-05-13 22:41:13 +0530

    Added metadata definitions for third-party and workforce navigation

---
+++ nav/metadata/third-party.meta.ts
@@ -0,0 +1,46 @@
+import { Handshake, HardHat, ClipboardList, Package } from 'lucide-react';
+import type { MetadataRegistry } from '../types';
+
+export const thirdPartyMetadata = {
+  'third-party': {
+    label: 'Third Party',
+    icon: Handshake,
+    nonInteractive: true,
+    order: 8,
+  },
+
+  'third-party-labour': {
+    label: 'Labour',
+    icon: HardHat,
+    order: 1,
+  },
+  'third-party-labour-new': { label: 'New Labour', sidebarHidden: true },
+  'third-party-labour-[id]': { label: 'Labour', sidebarHidden: true },
+  'third-party-labour-[id]-edit': { label: 'Edit', sidebarHidden: true },
+
+  'third-party-sub-contracts': {
+    label: 'Sub-Contracts',
+    icon: ClipboardList,
+    order: 2,
+  },
+  'third-party-sub-contracts-new': {
+    label: 'New Sub-Contract',
+    sidebarHidden: true,
+  },
+  'third-party-sub-contracts-[id]': {
+    label: 'Sub-Contract',
+    sidebarHidden: true,
+  },
+  'third-party-sub-contracts-[id]-edit': {
+    label: 'Edit',
+    sidebarHidden: true,
+  },
+
+  'third-party-vendors': {
+    label: 'Vendors',
+    icon: Package,
+    order: 3,
+  },
+  'third-party-vendors-new': { label: 'New Vendor', sidebarHidden: true },
+  'third-party-vendors-[id]': { label: 'Vendor', sidebarHidden: true },
+} satisfies MetadataRegistry;
