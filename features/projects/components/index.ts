0d0745580221d862ac515fa1965f7cf43d26aed0
Abin K Johny <abinkjohny2011@gmail.com>
2026-05-14 19:04:37 +0530

    feat: Add missing exports for IssueOverviewTab, ProjectOverview, ProjectCharts, and TaskCharts components

--- features/projects/components/index.ts
+++ features/projects/components/index.ts
@@ -1,4 +1,6 @@
 export { ProjectEditForm } from './project-edit-form';
+export { ProjectOverview } from './project-overview';
+export { ProjectCharts } from './project-charts';
 export { TeamMembersSection } from './team-members-section';
 export { AttachmentsSection } from './attachments-section';
 export { AttachmentsUploader } from './attachments-uploader';
