c9ac73b20830d063f74230c521c266166effce73
Abin K Johny <abinkjohny2011@gmail.com>
2026-07-15 22:43:49 +0530

    refactor: migrate site transfer components to use '@tornotron/echno-core' imports and add TransferTable and TransferRow components

--- features/site-transfers/components/index.ts
+++ features/site-transfers/components/index.ts
@@ -1,6 +1,8 @@
 export * from './site-transfer-alert-dialogs';
 export * from './site-transfer-items-card';
 export * from './site-transfer-locations-card';
+export * from './transfer-table';
+export * from './transfer-row';
 export { SiteTransferForm, SITE_TRANSFER_FORM_ID } from './site-transfer-form';
 export type {
   SiteTransferItemRow,
