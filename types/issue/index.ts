8207cdae7320d8da5b020bc30c4e5cb3530d8a2d
Abin K Johny <abinkjohny2011@gmail.com>
2026-07-15 22:06:22 +0530

    refactor(issue): remove unused issue & issue comment types and related interfaces

--- types/issue/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './issue-comment';
-export * from './issue-files';
-export * from './issue-status';
-export * from './issue-type';
-export * from './issue';
-export * from './issue-create';
-export * from './issue-update';
