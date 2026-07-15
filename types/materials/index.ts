d291c2a1ed331b30d719e52b4add1b1f6403fadd
Abin K Johny <abinkjohny2011@gmail.com>
2026-07-15 22:04:14 +0530

    refactor(materials): remove unused material types and interfaces

--- types/materials/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './material';
-export * from './material-create';
-export * from './material-update';
-export * from './material-consumption';
-export * from './material-consumption-create';
-export * from './enum';
