b60e8f41c8107b7996bd8f7ef360e2be9f112ca5
Abin K Johny <abinkjohny2011@gmail.com>
2026-07-15 22:26:34 +0530

    refactor: update employee hook import to use '@tornotron/echno-core'

--- app/users/dashboard/finance/expenses/[id]/page.tsx
+++ app/users/dashboard/finance/expenses/[id]/page.tsx
@@ -2,7 +2,7 @@
 
 import { use } from 'react';
 import { useExpenseById } from '@/hooks/expenses';
-import { useEmployees } from '@/hooks/employee';
+import { useEmployees } from '@tornotron/echno-core/employee/hooks';
 import { Button } from '@/components/shadcn/button';
 import { Badge } from '@/components/shadcn/badge';
 import {
