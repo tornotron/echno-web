4bd5ab3062160a67c3c44fe8cbef91b4e4b370fd
Abin K Johny <abinkjohny2011@gmail.com>
2026-07-15 22:04:39 +0530

    refactor(materials): remove unused materials hooks and service files

--- hooks/materials/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './material-keys';
-export * from './use-materials';
-export * from './use-materials-mutations';
--- hooks/materials/material-keys.ts
+++
@@ -1,9 +0,0 @@
-export const materialsKeys = {
-  all: ['materials'] as const,
-  lists: () => [...materialsKeys.all, 'list'] as const,
-  detail: (id: number) => [...materialsKeys.all, 'detail', id] as const,
-  stock: (id: number) => [...materialsKeys.all, 'stock', id] as const,
-  search: (name: string) => [...materialsKeys.all, 'search', name] as const,
-  paginated: (pageNo: number, pageSize: number) =>
-    [...materialsKeys.all, 'paginated', { pageNo, pageSize }] as const,
-};
