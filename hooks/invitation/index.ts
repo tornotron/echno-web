67b0b25d38d14d39378479bdd34e1e3b297af064
Abin K Johny <abinkjohny2011@gmail.com>
2026-07-15 22:08:28 +0530

    refactor(invitation): remove unused invitation hooks and service files

--- hooks/invitation/index.ts
+++
@@ -1,7 +0,0 @@
-export { useInvitationsByProject, useInvitationById } from './use-invitation';
-
-export {
-  useGenerateInviteCode,
-  useDeleteInviteCode,
-  useJoinWithInviteCode,
-} from './use-invitation-mutations';
