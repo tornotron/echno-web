8c33bf31dbe49e884c4af691abef24357891be13
Abin K Johny <abinkjohny2011@gmail.com>
2026-07-15 22:05:48 +0530

    refactor(labour): remove unused labour hooks and service files

--- hooks/labour/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './labour-keys';
-export * from './use-labour';
-export * from './use-labour-mutations';
--- hooks/labour/labour-keys.ts
+++
@@ -1,5 +0,0 @@
-export const labourKeys = {
-  all: ['labour'] as const,
-  lists: () => [...labourKeys.all, 'list'] as const,
-  detail: (id: number) => [...labourKeys.all, 'detail', id] as const,
-};
