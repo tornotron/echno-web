b5d554c0387f4e41bbd8018f99003352006d71e1
Abin K Johny <abinkjohny2011@gmail.com>
2026-07-15 22:05:31 +0530

    refactor(labour): remove unused labour types and interfaces

--- types/labour/index.ts
+++
@@ -1,5 +0,0 @@
-export type { Labour } from './labour';
-export { parseLabour } from './labour';
-export type { LabourCreateRequest } from './labour-create';
-export type { LabourUpdateRequest } from './labour-update';
-export { EmploymentType, SkillLevel, LabourStatus } from './enums';
