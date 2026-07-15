1b5ec5fb6636c643ed7107ce8e10a8a4469275c6
Abin K Johny <abinkjohny2011@gmail.com>
2026-07-15 22:11:17 +0530

    refactor(employee): remove unused employee-related types and interfaces

--- types/employee/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './employee-status';
-export * from './employee';
-export * from './departments';
-export * from './org-role';
-export * from './employee-create';
-export * from './employee-update';
